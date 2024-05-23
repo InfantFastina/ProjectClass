056d2ca03a935596fc86542cbe8bf0d0f26619f1
InfantFastina <infantfastina@gmail.com>
2024-05-23 20:09:42 +0530

    New Sample Project

---
+++ src/test/java/org/sample/SampleA.java
@@ -0,0 +1,20 @@
+package org.sample;
+
+public class SampleA {
+	
+	public void Course1() {
+           System.out.println("Java");
+
+
+	}
+	public void Course2() {
+
+         System.out.println("Python");
+	}
+	public void Course3() {
+
+            System.out.println("SQL");
+
+	}
+
+}
